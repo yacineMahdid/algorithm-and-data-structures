0e5a3c38c0e79b264282d6b5c85e5e35ecf3717f
Bl0101 <yacine.mahdid@live.com>
2018-11-15 20:32:51 -0500

    FIX Pivot quick_sort

--- source/algorithms/sort/quick_sort.py
+++ source/algorithms/sort/quick_sort.py
@@ -2,11 +2,15 @@
 def quick_sort(items):
     print("Do the quick sort")
 
+
 def sort(items,low,high):
-    pivot = 0;
     if high > low:
-        partition(items, low, high, pivot)
+        pivot = partition(items, low, high)
         sort(items, low, pivot - 1)
         sort(items, pivot + 1, high)
 
-def partition(items, low, high, pivot):
+
+def partition(items, low, high):
+    pivot = 0
+
+    return pivot
