215991d50f669b8b57041f9bb662d05e38d5a7f2
Bl0101 <yacine.mahdid@live.com>
2018-11-15 20:29:23 -0500

    ADD the sort algorithm for quick_sort

--- source/algorithms/sort/quick_sort.py
+++ source/algorithms/sort/quick_sort.py
@@ -1,3 +1,12 @@
 
 def quick_sort(items):
     print("Do the quick sort")
+
+def sort(items,low,high):
+    pivot = 0;
+    if high > low:
+        partition(items, low, high, pivot)
+        sort(items, low, pivot - 1)
+        sort(items, pivot + 1, high)
+
+def partition(items, low, high, pivot):
