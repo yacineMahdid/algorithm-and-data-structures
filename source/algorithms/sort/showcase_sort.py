e24ffe4df76df0c42a74ef9ad66d3a5d8ef4223d
Bl0101 <yacine.mahdid@live.com>
2018-11-18 14:44:57 -0500

    ADD begining of sorting unit testing

---
+++ source/algorithms/sort/showcase_sort.py
@@ -0,0 +1,5 @@
+from merge_sort import merge_sort
+from quick_sort import quick_sort
+
+def sort(items):
+    return merge_sort(items)
---
+++ test/test_sort.py
@@ -0,0 +1,10 @@
+import unittest
+from showcase_sort import sort
+
+class TestSorting(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
