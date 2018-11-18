9a2bf70c334021d85fdc1b083a98a5dbfd036c0f
Bl0101 <yacine.mahdid@live.com>
2018-11-18 15:24:14 -0500

    ADD structure to the math folder

---
+++ source/algorithms/math/multiplication.py
@@ -0,0 +1,7 @@
+# Yacine Mahdid - 2018-11-18 (Foundation of Algorithms)
+# Collection of algorithm for multiplications written from scratch
+
+
+def strassen(matrix_1, matrix_2):
+
+    print("Here we should multiply matrix one and two using strassen algorithm")
